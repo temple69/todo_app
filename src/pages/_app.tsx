b3d1d2be8a18ab3aa44ec5bd7831a495e6eb0fc2
temple69 <utemple169@gmail.com>
2023-09-11 16:03:13 +0100

    root file for next.js project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import Home from "@/Components/Home";
+
 import Layout from "@/Components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    //General Layout
     <Layout>
       <Component {...pageProps} />
     </Layout>
