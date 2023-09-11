99433539f4fa8e92844667ae7064935778084c5f
temple69 <utemple169@gmail.com>
2023-09-11 16:06:04 +0100

    folder for types used in the project

--- src/types/task_types.ts
+++ src/types/task_types.ts
@@ -1,21 +1,35 @@
+// Declaring types for the task component
 export type TaskDetails = {
   task_description: string;
   task_date: string;
   start_time: string;
   end_time: string;
 };
+//Decalring types for the todo component
 export type singleTodoType = {
   id: string;
   title: string;
   start_time: string;
   end_time: string;
   date: string;
-  completed?:boolean
+  completed?:boolean | undefined;
 };
+//Declaring types for the todoContext
 export interface todoContextType{
   todos: singleTodoType[];
-  singleTodo: {};
-  editedTodo: {};
-  findByIdHandler: (mode: string, id: string) => void;
+  singleTodo: singleTodoType;
+  editedTodo:singleTodoType;
+  findByIdHandler: (mode: string, id: string) => singleTodoType;
+  addToTaskListHandler: (task: singleTodoType,id:string) => void;
+  modalState:boolean;
+  openModalHandler: () => void;
+  closeModalHandler: () => void;
+  filterByDateHandler: (date: string) => void;
+  filteredTodosDate: singleTodoType[];
   
 };
+//Declaring types for the pagination component
+  export type PaginationType={
+    pageCount:number
+    handlePageClick:(page: {selected: number;})=>void,
+}
